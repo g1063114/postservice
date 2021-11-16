f53d05b606c3923e5bd4734797d8c090905d9a1c
jinho <g1063114@naver.com>
2021-11-16 14:51:18 +0900

    post google login

--- src/main/java/com/jeeno/postservice/web/HomeController.java
+++ src/main/java/com/jeeno/postservice/web/HomeController.java
@@ -1,13 +1,28 @@
 package com.jeeno.postservice.web;
 
+import com.jeeno.postservice.web.dto.SessionUser;
+import com.jeeno.postservice.web.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if( user != null ){
+            model.addAttribute("userName", user.getName());
+        }
         return "home";
     }
 }
