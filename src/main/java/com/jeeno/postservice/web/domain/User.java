74a8f41715c49b8c676a94fb905425fae625b73b
jinho <g1063114@naver.com>
2021-11-17 12:25:32 +0900

    aws ec2 rds 연동 google login error

--- src/main/java/com/jeeno/postservice/web/domain/User.java
+++ src/main/java/com/jeeno/postservice/web/domain/User.java
@@ -12,7 +12,7 @@ import javax.persistence.*;
 public class User extends BaseTimeEntity{
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
